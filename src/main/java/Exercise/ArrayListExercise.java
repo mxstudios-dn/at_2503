47a59bb61a4a57c2cb2fd7a877cfc838203f5b01
Hanh Dung <94171909+Hanhdung007@users.noreply.github.com>
2025-11-06 23:15:09 +0700

    Dung Student Array list

--- src/main/java/Exercise/ArrayListExercise.java
+++ src/main/java/Exercise/ArrayListExercise.java
@@ -1,5 +1,30 @@
 package Exercise;
 
 public class ArrayListExercise {
-    
+    int id;
+    String studentName;
+    double score;
+
+    public ArrayListExercise(int id, String studentName, double score) {
+        this.id = id;
+        this.studentName = studentName;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    //Display student list
+    public void displayStudent(){
+        System.out.println("ID: " + id + ", Name: " + studentName + ", Score: " + score);
+    }
 }
