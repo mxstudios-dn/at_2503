9de642dbef5a7c4e6bbba2b669484ae2f17e7b6f
VuDucLan <128978863+USB419003@users.noreply.github.com>
2025-11-12 19:15:20 +0700

    Manage xml files
    Added catalog.xml, XMLHelper.java, Book.java.
    Added functions in Main.java

--- src/main/java/vn/vti/academy/Main.java
+++ src/main/java/vn/vti/academy/Main.java
@@ -4,6 +4,27 @@ package vn.vti.academy;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        String filePath = "src/ catalog.xml";
+        ArrayList<Book> books = XMLHelper.loadBooks(filePath);
+
+        System.out.println("== All books loaded ==");
+        books.forEach(System.out::println);
+
+        System.out.println("\n== Books with price > 20 ==");
+        books.stream()
+                .filter(book -> book.getPrice() > 20)
+                .forEach(System.out::println);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter author name to search: ");
+        String searchAuthor = sc.nextLine();
+
+        System.out.println("\n== Search results ==");
+        books.stream()
+                .filter(book -> book.getAuthor().equalsIgnoreCase(searchAuthor))
+                .forEach(System.out::println);
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
