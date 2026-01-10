b39dab955825de704639f07d7ae463e5b5771840
mxstudios-dn <mxstudios.dn@gmail.com>
2026-01-10 22:06:35 +0700

    Update CucumberHooks.java

--- src/test/java/step_definitions/CucumberHooks.java
+++ src/test/java/step_definitions/CucumberHooks.java
@@ -1,6 +1,11 @@
 package step_definitions;
 
 import io.cucumber.java.*;
+import io.qameta.allure.Allure;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import core.BasePage;
 import core.DriverManager;
 import core.ShareValueManager;
@@ -8,6 +13,9 @@ import core.ShareValueManager;
 
 public class CucumberHooks extends BasePage {
 
+    /** 
+     * Hook to run before each scenario.
+     */
     @Before()
     public void setUp() {
         // Code to run before each scenario
@@ -23,49 +31,51 @@ public class CucumberHooks extends BasePage {
         }
     }
 
+    /** 
+     * Hook to run after each scenario.
+     * @param scenario The scenario that just ran.
+     */
     @After()
     public void tearDown(Scenario scenario) {
         // Code to run after each scenario
         logger.info("[Cucumber Hooks] After scenario hook executed.");
+
+        // Calculate and log scenario duration
         ShareValueManager.setValue("SCENARIO_END_TIME", String.valueOf(System.currentTimeMillis()));
         Long startTime = Long.parseLong(ShareValueManager.getValue("SCENARIO_START_TIME"));
         Long endTime = Long.parseLong(ShareValueManager.getValue("SCENARIO_END_TIME"));
         Long duration = (endTime - startTime) / 1000;
         logger.info("[Cucumber Hooks] Scenario '{}' executed in {} s", scenario.getName(), duration);
+
         // Check if scenario failed and capture screenshot
         if (scenario.isFailed()) {
             logger.error("Test Failed: " + scenario.getName());
-            if (DriverManager.getDriver() != null) {
-                captureScreenshot(scenario.getName() + "_onTestFailure");
+        }
+        if (DriverManager.getDriver() != null) {
+            String screenshotPath = captureScreenshot(scenario.getName() + "_onTestFailure");
+            
+            try {
+                byte[] screenshotBytes = Files.readAllBytes(Paths.get(screenshotPath));
+                Allure.addAttachment("Screenshot on Failure", "image/png", new java.io.ByteArrayInputStream(screenshotBytes), "png");
+            } catch (java.io.IOException e) {
+                logger.error("Failed to attach screenshot to Allure report", e);
             }
         }
+
+        // Quit browser
         DriverManager.quit();
     }
 
-    @BeforeStep()
-    public void beforeStep(Scenario scenario) {
-        // Code to run before each step
-        logger.info("[Cucumber Hooks] Before step hook executed. ["+ scenario.getName()+"]");
-    }
-
-    @AfterStep()
-    public void afterStep(Scenario scenario) {
-        // Code to run after each step
-        logger.info("[Cucumber Hooks] After step hook executed. ["+ scenario.getName()+"]");  
-        logger.info("[Cucumber Hooks] Scenario status: [" + scenario.getStatus().toString() + "]");
-    }
-
-    @BeforeAll()
-    public static void beforeAll() {
-        // Code to run once before all scenarios
-        System.out.println("[Cucumber Hooks] Before all scenarios hook executed.");
-    }
+    // @BeforeAll()
+    // public static void beforeAll() {
+    //     // Code to run once before all scenarios
+    //     System.out.println("[Cucumber Hooks] Before all scenarios hook executed.");
+    // }
 
     // @AfterAll
     // public static void afterAll() {
     //     // Code to run once after all scenarios
     //     logger.info("[Cucumber Hooks] After all scenarios hook executed.");
-    //     DriverManager.quitDriver();
     // }
 
 }
